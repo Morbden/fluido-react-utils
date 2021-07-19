77ec93db658b6833ae25f5cc9571a4636be2969e
Renato Rodrigues <ren.gsg@gmail.com>
2021-07-19 14:58:56 -0300

    atualização adição do fetch hook

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,57 +1,156 @@
-import merge from 'deepmerge'
+import { useEffect, useState } from 'react'
 
-interface FetchAPIGeneric {
-  [key: string]: any
+type RequestConfig = RequestInit & {
+  token?: string
+  basicToken?: string
 }
 
-export const fetchAPI = async <T = FetchAPIGeneric>(
-  url: RequestInfo,
-  options: RequestInit = {},
-) => {
-  return fetch(
-    url,
-    merge(
-      {
-        headers: {
-          Accept: 'application/json, text/plain',
-          'Content-Type': 'application/json',
-        },
-        credentials: 'same-origin',
-        redirect: 'follow',
+type RequestHookConfig = RequestConfig & {
+  blockRequest?: boolean
+  dependencies?: any[]
+}
+
+type ErrorResult = {
+  status?: number
+  statusText?: string
+  exception?: Error
+}
+
+interface TypedMap<T = any> {
+  [key: string]: T
+}
+
+export function useFetch<T = any>(
+  request: RequestInfo,
+  config?: RequestHookConfig,
+) {
+  const [trigger, setTrigger] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState<T | null>(null)
+  const [textData, setTextData] = useState<string | null>(null)
+  const [error, setError] = useState<ErrorResult | null>(null)
+  const {
+    blockRequest,
+    headers,
+    token,
+    basicToken,
+    dependencies = [],
+    ...extraConfig
+  } = config || {}
+
+  useEffect(() => {
+    setData(null)
+    setTextData(null)
+    setError(null)
+
+    if (blockRequest) return
+
+    const controller = new AbortController()
+
+    setLoading(true)
+    fetch(request, {
+      signal: controller.signal,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          (token && `Bearer {token}`) ||
+          (basicToken && `Basic ${basicToken}`) ||
+          '',
+        ...headers,
       },
-      options,
-    ),
-  ).then(async (response) => {
+      ...extraConfig,
+    })
+      .then(async (response) => {
+        setError({
+          status: response.status,
+          statusText: response.statusText,
+        })
+        const value = await response.text()
+        setTextData(value)
+        try {
+          const jsonValue = JSON.parse(value)
+          setData(jsonValue)
+        } catch (_) {}
+      })
+      .catch((err) => {
+        console.error(err)
+        setError({
+          exception: err,
+        })
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+    return () => {
+      controller.abort()
+    }
+  }, [trigger, blockRequest].concat(dependencies))
+
+  return {
+    data,
+    textData,
+    error,
+    loading,
+    reRequest: () => setTrigger((i) => i++),
+  }
+}
+
+export const fetchAPI = async (
+  url: RequestInfo,
+  options: RequestConfig = {},
+) => {
+  const { headers, token, basicToken, ...extraConfig } = options || {}
+
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization:
+        (token && `Bearer {token}`) ||
+        (basicToken && `Basic ${basicToken}`) ||
+        '',
+      ...headers,
+    },
+    ...extraConfig,
+  }).then(async (response) => {
     const jsonType = /application\/json/g.test(
       response.headers.get('content-type') || '',
     )
-    const text = await response.text()
+    const dataText = await response.text()
 
     if (jsonType) {
       return Promise.resolve({
-        status: response.status,
-        statusText: response.statusText,
-        json: JSON.parse(text),
-        text,
+        error: {
+          status: response.status,
+          statusText: response.statusText,
+        },
+        data: JSON.parse(dataText),
+        dataText,
       })
     } else {
       return Promise.resolve({
-        status: response.status,
-        statusText: response.statusText,
-        json: null,
-        text,
+        error: {
+          status: response.status,
+          statusText: response.statusText,
+        },
+        data: null,
+        dataText,
       })
     }
   })
 }
 
-export const genDefaultFetchAPI = <T = FetchAPIGeneric>(data: T) => {
+export const genDefaultFetchAPI = <T = TypedMap>(data: T) => {
   return {
     status: 200,
     statusText: 'OK',
-    ...(typeof data === 'string' ? { text: data } : { json: data }),
+    ...(typeof data === 'string'
+      ? { dataText: data, data: null }
+      : { data: data, dataText: JSON.stringify(data) }),
   }
 }
 
-export const fetcherSWR = (url: RequestInfo, options: RequestInit = {}) =>
+export const fetcherSWR = (url: RequestInfo, options: RequestConfig = {}) =>
   fetchAPI(url, options)
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-import { fetchAPI, fetcherSWR, genDefaultFetchAPI } from './fetch'
-import loadI18Next from './i18n'
-import { promiseDelay, simplifyId } from './tools'
+export { fetchAPI, fetcherSWR, genDefaultFetchAPI } from './fetch'
+export { promiseDelay, simplifyId } from './tools'
 
-export {
-  fetchAPI,
-  fetcherSWR,
-  genDefaultFetchAPI,
-  loadI18Next,
-  promiseDelay,
-  simplifyId,
-}
+import loadI18Next from './i18n'
+
+export { loadI18Next }
